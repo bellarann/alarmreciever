70898560b755345494ef54df5f4718cc0f1b3843
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-12 08:45:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    while (true) {
+        for (let index = 0; index < 5; index++) {
+            music.playMelody("B E B E B E B E ", 296)
+        }
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Diamond)
+            basic.showIcon(IconNames.SmallDiamond)
+        }
+    }
+})
+radio.setGroup(8148)
+basic.forever(function () {
+	
+})
