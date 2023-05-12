a8150110072ff2f0a4d0eae832f83a16d88a0d1f
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-12 09:14:51 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedString(function (receivedString) {
     while (true) {
         for (let index = 0; index < 5; index++) {
             music.playMelody("B E B E B E B E ", 296)
@@ -10,6 +10,3 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 radio.setGroup(8148)
-basic.forever(function () {
-	
-})
