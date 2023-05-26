85dde8c56bd583dabe20f24a94ad3bc0cc045c13
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-26 18:33:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,6 @@ radio.onReceivedString(function (receivedString) {
         for (let index = 0; index < 5; index++) {
             music.playMelody("B E B E B E B E ", 296)
         }
-        for (let index = 0; index < 4; index++) {
-            basic.showIcon(IconNames.Diamond)
-            basic.showIcon(IconNames.SmallDiamond)
-        }
     } else {
         basic.clearScreen()
     }
